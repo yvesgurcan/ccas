abb756442ba908c3b5e6ff3ad29c34767b254069
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-05-24 22:13:59 -0700

    constant for supplier APIs

--- api/index.js
+++ api/index.js
@@ -5,6 +5,9 @@ const app = express();
 const hostname = 'localhost';
 const port = 3000;
 
+const acmeApiKey = 'cascade.53bce4f1dfa0fe8e7ca126f91b35d3a6';
+const rainierStorefront = 'ccas-bb9630c04f';
+
 app.post('/order', (req, res) => {
   const { make, model, packageLevel, customer_id } = req.body;
   res.send();
