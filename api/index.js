494fd602e8a54f3e6cafce8a1c759e98c75b0a3e
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-05-24 20:59:23 -0700

    endpoints

--- api/index.js
+++ api/index.js
@@ -1,1 +1,18 @@
-// TODO
+const express = require('express');
+
+const app = express();
+
+const hostname = 'localhost';
+const port = 3000;
+
+app.post('/order', (req, res) => {
+  res.send();
+});
+
+app.get('/orders', (req, res) => {
+  res.send();
+});
+
+app.listen(port, hostname, () => {
+  console.log(`CCAS API listening at ${hostname}:${port}`);
+});
