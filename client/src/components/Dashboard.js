2c0bcf7e105f5d073ad1c6db94ac51738023fe98
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-05-26 21:26:12 -0700

    Dashboard UI - Redux implementation

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,39 +1,62 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import mapStateToProps from '../mapStateToProps';
+import mapDispatchToProps from '../mapDispatchToProps';
 
-const orders = [];
+import {
+    LOADING,
+    SUCCESS,
+    ERROR,
+} from '../reducers/statusTypes';
 
 class Dashboard extends Component {
+    componentDidMount() {
+        this.props.fetchOrders();
+    }
     render() {
+        const {
+            status,
+            orders = [],
+            error,
+        } = this.props.dashboard;
         return (
             <div>
                 <div>
                     <h1>Dashboard</h1>
                 </div>
                 <Link to="/order/new">Add Order</Link>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Make</th>
-                            <th>Model</th>
-                            <th>Package</th>
-                            <th>Customer ID</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {orders.map(order => (
-                            <tr key={order._id}>
-                                <td>{order.make}</td>
-                                <td>{order.model}</td>
-                                <td>{order.package}</td>
-                                <td>{order._id}</td>
+                <div className="feedback">
+                    { status === LOADING && 'Loading...' }
+                    { status === ERROR && `An error occurred: ${error}` }
+                </div>
+                { status === SUCCESS && 
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Make</th>
+                                <th>Model</th>
+                                <th>Package</th>
+                                <th>Supplier</th>
+                                <th>Customer ID</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {orders.map(order => (
+                                <tr key={order._id}>
+                                    <td>{order.make}</td>
+                                    <td>{order.model}</td>
+                                    <td>{order.packageLevel}</td>
+                                    <td>{order.supplier}</td>
+                                    <td>{order._id}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                }
             </div>
         );
     }
 }
 
-export default Dashboard;
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
