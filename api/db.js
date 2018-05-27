2c47b58d518b7d8e04ba64913b077e2f302dc551
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-05-26 21:23:19 -0700

    indentations

--- api/db.js
+++ api/db.js
@@ -2,32 +2,32 @@ const mongoose = require('mongoose');
 
 const address = 'mongodb://localhost/ccas';
 const connect = function () {
-  mongoose.connect(address).then(
-    () => null,
-    error => console.error('An error occurred while connecting to the database:\n', error)
-  );
+    mongoose.connect(address).then(
+        () => null,
+        error => console.error('An error occurred while connecting to the database:\n', error)
+    );
 };
 
 const Schema = mongoose.Schema;
 const OrderSchema = new Schema({
-  make: String,
-  model: String,
-  packageLevel: String,
-  customerId: Number,
-  supplier: String,
-  supplierOrderId: String,
+    make: String,
+    model: String,
+    packageLevel: String,
+    customerId: Number,
+    supplier: String,
+    supplierOrderId: String,
 });
 const Order = mongoose.model('Order', OrderSchema);
 
 module.exports = {
-  fetchOrders: () => Promise.resolve(connect()).then(() => Order.find()),
-  insertOrder: (orderData) => Promise.resolve(connect()).then(() => new Order(orderData).save()),
-  addSupplierOrderId: (orderId, supplierOrderId) =>
-    Promise.resolve(connect()).then(() =>
-      Order.update(
-      { _id: orderId },
-      { $set: { supplierOrderId } },
-      null
-    )
-  ),
+    fetchOrders: () => Promise.resolve(connect()).then(() => Order.find()),
+    insertOrder: (orderData) => Promise.resolve(connect()).then(() => new Order(orderData).save()),
+    addSupplierOrderId: (orderId, supplierOrderId) =>
+        Promise.resolve(connect()).then(() =>
+            Order.update(
+            { _id: orderId },
+            { $set: { supplierOrderId } },
+            null
+        )
+    ),
 };
--- api/supplierRequest.js
+++ api/supplierRequest.js
@@ -1,52 +1,51 @@
 const request = require('superagent');
 
 const suppliers = {
-  acme: {
-    api: 'http://localhost:3050/acme/api/v45.1',
-    apiKey: 'cascade.53bce4f1dfa0fe8e7ca126f91b35d3a6',
-  },
-  rainier: {
-    api: 'http://localhost:3051/rainier/api/v10.0',
-    storefront: 'ccas-bb9630c04f',
-  }
+    acme: {
+        api: 'http://localhost:3050/acme/api/v45.1',
+        apiKey: 'cascade.53bce4f1dfa0fe8e7ca126f91b35d3a6',
+    },
+    rainier: {
+        api: 'http://localhost:3051/rainier/api/v10.0',
+        storefront: 'ccas-bb9630c04f',
+    }
 };
 
 const extractSupplierOrderId = function (supplier, apiRes) {
   const {
-    order,
-    order_id,
-    orderId,
+      order,
+      order_id,
+      orderId,
   } = apiRes.body;
   return order || order_id || orderId || null;
 };
 
 module.exports = function (supplier, order) {
-  console.log(`Sending request to supplier '${supplier}'.`);
-  switch (supplier) {
-    default: {
-      throw Error('Invalid supplier.');
-    }
-    case 'acme': {
-      return request.post(`${suppliers[supplier].api}/order`)
-        .set('Api-Key', suppliers[supplier].apiKey)
-        .set('Content-Type', 'application/x-www-form-urlencoded')
-        .send(order)
-        .then((apiRes) => extractSupplierOrderId(supplier, apiRes));
-    }
-    case 'rainier': {
-      return request.get(`${suppliers[supplier].api}/nonce_token`)
-        .set('Storefront', suppliers[supplier].storefront)
-        .then((tokenRes) => {
-          const { nonce_token } = tokenRes.body;
-          if (!nonce_token) {
-            throw new Error('Supplier API did not provide a token.');
-          }
-          return request.post(`${suppliers[supplier].api}/request_customized_model`)
-          .set('Token', nonce_token)
-          .send(order)
-          .then((apiRes) => extractSupplierOrderId(supplier, apiRes));
+    console.log(`Sending request to supplier '${supplier}'.`);
+    switch (supplier) {
+        default: {
+            throw Error('Invalid supplier.');
         }
-        );
+        case 'acme': {
+            return request.post(`${suppliers[supplier].api}/order`)
+                .set('Api-Key', suppliers[supplier].apiKey)
+                .set('Content-Type', 'application/x-www-form-urlencoded')
+                .send(order)
+                .then((apiRes) => extractSupplierOrderId(supplier, apiRes));
+        }
+        case 'rainier': {
+            return request.get(`${suppliers[supplier].api}/nonce_token`)
+                .set('Storefront', suppliers[supplier].storefront)
+                .then((tokenRes) => {
+                    const { nonce_token } = tokenRes.body;
+                    if (!nonce_token) {
+                      throw new Error('Supplier API did not provide a token.');
+                    }
+                    return request.post(`${suppliers[supplier].api}/request_customized_model`)
+                    .set('Token', nonce_token)
+                    .send(order)
+                    .then((apiRes) => extractSupplierOrderId(supplier, apiRes));
+                });
+        }
     }
-  }
 };
