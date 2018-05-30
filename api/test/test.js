7306f5540fe0e6fa80d3bc4d959774f7b0638b05
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-05-29 22:40:32 -0700

    add test

--- api/test/test.js
+++ api/test/test.js
@@ -1,13 +1,30 @@
 const app = require('../');
 const db = require('../db');
+const request = require('superagent');
+const mockSuperagent = require('superagent-mock');
 const utils = require('./utils');
 const assert = require('assert');
+require('dotenv').load();
+
 const chai = require('chai');
 
 const expect = chai.expect;
 chai.use(require('chai-json-schema'));
 
+const { HOST_TEST, PORT_TEST, ROOT_TEST } = process.env;
 
+const host = HOST_TEST || 'localhost';
+const port = PORT_TEST || 3001;
+const root = ROOT_TEST || '/api';
+const apiUrl = `http://${host}:${port}${root}`;
+
+describe('service API', function () {
+    describe('GET /orders', function () {
+        it('should return a list of orders', function (done) {
+            done();
+        })
+    })
+})
 
 describe.skip('database', function () {
     describe('insertOrder()', function () {
