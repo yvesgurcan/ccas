c38665390995272de6360ef9ff1e6a030e036298
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-05-30 20:58:34 -0700

    callback issue

--- api/createJSONFile.js
+++ api/createJSONFile.js
@@ -3,9 +3,8 @@ const log = require('./log');
 
 module.exports = function (path, orderData) {
     log('Creating static JSON file.');
-    return Promise.resolve(fs.writeFile(`${path}.json`, JSON.stringify(orderData, null, '\t')))
-        .catch(error => {
-            log(error);
+    return Promise.resolve(fs.writeFile(`${path}.json`, JSON.stringify(orderData, null, '\t'), error => {
+            if (error) log(error);
             return null;
-        });
+        }));
 };
